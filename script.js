4b2480af31536918d19d3e71087177e0270bdb60
pragati-agnihotri <pragatiagnihotri25@gmail.com>
2023-09-12 18:39:44 +0530

    done

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+const secondHand = document.getElementById('second-hand');
+const minuteHand = document.getElementById('minute-hand');
+const hourHand  = document.getElementById('hour-hand');
+
+
+function clockTick(){
+	const date = new Date();
+	const seconds = date.getSeconds() /60;
+	const minutes = (seconds + date.getMinutes()) /60;
+	const hours = (minutes + date.getHours()) /12;
+
+
+	rotateClockHand(secondHand, seconds);
+	rotateClockHand(minuteHand, minutes);
+    rotateClockHand(hourHand, hours);
+}
+
+
+function rotateClockHand(element,rotation){
+	element.style.setProperty('--rotate',rotation * 360);
+	
+}
+window.setInterval(clockTick, 1000);
+
+
+
+
+
+
